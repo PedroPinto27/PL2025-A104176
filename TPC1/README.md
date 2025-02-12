4da313da39e8815dea60aa059cc8d406c175b692
a104176 <a104176@alunos.uminho.pt>
2025-02-12 01:30:36 +0000

    Tpc1

--- TPC1/README.md
+++ TPC1/README.md
@@ -1,1 +1,31 @@
-# TPC1
+# TPC1 - Somador ON/OFF/=
+
+**Data:** 2024-02-12
+
+**Autor:** Pedro Filipe Maneta Pinto
+
+**Número Mecanográfico** A104176
+
+## Sobre o TPC
+
+Este projeto implementa um somador que funciona seguindo os comandos "on" e "off" dentro de uma sequência de números e operadores. 
+O objetivo é processar uma mensagem de entrada contendo instruções e calcular a soma acumulada conforme os estados ativados ou desativados. 
+
+**"*ON*": Ativa a soma.**
+
+**"*OFF*": Desativa a soma.**
+
+**"=": Imprime a soma atual.**
+
+## Exemplo
+
+Uma mensagem por exemplo: **"10a-5b=on3c4d=off6e-2f=on8g-10h=on5i="**
+
+Devolve o seguinte *Output*:
+- 5
+- 12
+- 12
+- 10
+- 15
+
+
---
+++ TPC1/tpc1.py
@@ -0,0 +1,47 @@
+def soma_num(texto):
+    
+    somaTotal = 0
+    somador = True
+    i = 0
+    numTemp = ""
+    
+    while i < len(texto):
+        
+        if (texto[i:i+3].lower() == 'off'):
+            somador = False
+            i = i+2
+            
+        elif (texto[i:i+2].lower() == 'on'):
+            somador = True
+            i = i+1
+            
+        elif (texto[i] == '='):
+            print(somaTotal)
+            
+        elif (texto[i] == '-' and somador and i+1<len(texto)):
+            i=i+1
+            numTemp = '-'
+            while(i < len(texto) and texto[i].isdigit()):
+                numTemp += texto[i]
+                i = i+1
+            somaTotal = somaTotal + (int(numTemp))
+            numTemp = ""
+            continue
+        
+        elif (texto[i].isdigit() and somador):
+            numTemp += texto[i]
+            while(i+1 < len(texto) and texto[i+1].isdigit()):
+                numTemp += texto[i+1]
+                i = i+1
+            
+            somaTotal = somaTotal + (int(numTemp))
+            numTemp = ""
+            i = i+1
+            continue
+        
+        i = i+1
+        
+    return somaTotal
+
+message = "10a-5b=on3c4d=off6e-2f=on8g-10h=on5i="
+soma_num(message)
